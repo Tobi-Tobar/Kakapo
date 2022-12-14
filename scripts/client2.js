c0ee352452126e56f94f3b6060be2c78d0fbd39c
Tobi-Tobar <bianka.tobar@sjsu.edu>
2022-12-14 09:33:37 -0800

    hopefully this works cos fuck this shit

--- scripts/client2.js
+++ scripts/client2.js
@@ -115,7 +115,7 @@ let mesh;
 
 const loader = new GLTFLoader();
 loader.load(
-  "../../assets/Kakapo.glb",
+  "../assets/Kakapo.glb",
   function ( gltf ) {
     mesh = gltf.scene;
     mesh.scale.set(1.5,1.5,1.5);
@@ -130,7 +130,7 @@ let mesh1;
 
 const loader1 = new GLTFLoader();
 loader.load(
-  "../../assets/ground2.glb",
+  "../assets/ground2.glb",
   function ( gltf ) {
     mesh1 = gltf.scene;
     mesh1.scale.set(5,5,5);
@@ -324,7 +324,7 @@ loader.load(
   // Position plane geometry
   plane8.position.set(-20 , 31 , 17);
   plane8.rotation.set(0,2.5,0);
-  // Place plane geometry 
+  // Place plane geometry
   scene.add( plane8 );
 
   // Add Text under models
